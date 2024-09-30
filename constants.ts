089e83d2d6f74bd50d841a166aa05073737952f5
HackSmith010 <anshujaiswal119@gmail.com>
2024-09-30 22:39:56 +0530

    final commit

---
+++ constants.ts
@@ -0,0 +1,12 @@
+export const SWATCHES = [
+    "#ffffff",  //white
+    "#ff0000",  //red
+    "#ff4000",  //orange
+    "#ffff00",  //yellow
+    "#00ff00",  //green
+    "#0080ff",  //blue
+    "#ff0080",   //pink
+    "#a52a2a",  //brown
+    "#00008b",  //dark blue
+    "#006400",  //dark green
+]
---
+++ postcss.config.cjs
@@ -0,0 +1,14 @@
+module.exports ={
+    plugins: {
+        "postcss-preset-mantine":{},
+        "postcss-simple-vars":{
+            variables: {
+                'maintine-breakpoint-xs' : '36em',
+                'maintine-breakpoint-sm' : '48em',
+                'maintine-breakpoint-md' : '62em',
+                'maintine-breakpoint-lg' : '75em',
+                'maintine-breakpoint-xl' : '88em',
+            }
+        },
+    },
+};
